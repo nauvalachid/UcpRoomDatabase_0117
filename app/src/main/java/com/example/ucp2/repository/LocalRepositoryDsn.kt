b609c8b436110bd50f4f023897f5b843d5cab1f1
nauvalachid <nauvalachid@gmail.com>
2024-12-20 17:25:38 +0700

    Melengkapi LocalRepositoryDsn.kt

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryDsn.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryDsn.kt
@@ -1,7 +1,17 @@
 package com.example.ucp2.repository
 
 import com.example.ucp2.data.dao.DosenDao
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryDsn (
     private val dosenDao: DosenDao
-) :
+) : RepositoryDsn {
+    override suspend fun insertDsn(dosen: Dosen) {
+        dosenDao.insertDosen(dosen)
+    }
+
+    override fun getAllDsn(): Flow<List<Dosen>> {
+        return dosenDao.getAllDosen()
+    }
+}
