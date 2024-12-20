917ac7b9ff2e51dad4112d259b316a9af263bca5
nauvalachid <nauvalachid@gmail.com>
2024-12-20 17:31:40 +0700

    Implementasi kelas LocalRepositoryMk untuk operasi data MataKuliah

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryMk.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryMk.kt
@@ -1,4 +1,29 @@
 package com.example.ucp2.repository
 
-class LocalRepositoryMk {
+import com.example.ucp2.data.dao.MataKuliahDao
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMk (
+    private val mataKuliahDao: MataKuliahDao
+) : RepositoryMk {
+    override suspend fun insertMk(mataKuliah: MataKuliah) {
+        mataKuliahDao.insertMataKuliah(mataKuliah)
+    }
+
+    override fun getAllMk(): Flow<List<MataKuliah>> {
+        return mataKuliahDao.getAllMataKuliah()
+    }
+
+    override fun getMk(kode: String): Flow<MataKuliah> {
+        return mataKuliahDao.getMataKuliah(kode)
+    }
+
+    override suspend fun deleteMk(mataKuliah: MataKuliah) {
+        mataKuliahDao.deleteMataKuliah(mataKuliah)
+    }
+
+    override suspend fun updateMk(mataKuliah: MataKuliah) {
+        mataKuliahDao.updateMataKuliah(mataKuliah)
+    }
 }
