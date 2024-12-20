6e1ad003254c00e0d5a444da5dfa811d39e1ab92
nauvalachid <nauvalachid@gmail.com>
2024-12-20 17:36:10 +0700

    Implementasi InterfaceContainerApp dan kelas ContainerApp untuk inisialisasi repository

--- app/src/main/java/com/example/ucp2/dependenciesinjection/InterfaceContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/InterfaceContainerApp.kt
@@ -1,2 +1,22 @@
 package com.example.ucp2.dependenciesinjection
 
+import android.content.Context
+import com.example.ucp2.data.database.KrsDatabase
+import com.example.ucp2.repository.LocalRepositoryDsn
+import com.example.ucp2.repository.LocalRepositoryMk
+import com.example.ucp2.repository.RepositoryDsn
+import com.example.ucp2.repository.RepositoryMk
+
+interface InterfaceContainerApp {
+    val repositoryDsn: RepositoryDsn
+    val repositoryMk: RepositoryMk
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp{
+    override val repositoryDsn: RepositoryDsn by lazy {
+        LocalRepositoryDsn(KrsDatabase.getDatabase(context).dosenDao())
+    }
+    override val repositoryMk: RepositoryMk by lazy {
+        LocalRepositoryMk(KrsDatabase.getDatabase(context).matakuliahdao())
+    }
+}
