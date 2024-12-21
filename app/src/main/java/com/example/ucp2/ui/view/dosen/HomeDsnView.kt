cd284923ffb599c15eb513b696c0a65ed9721617
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:32:04 +0700

    Menambahkan Scaffold dengan TopAppBar, FloatingActionButton, dan BodyHomeDsnView pada HomeDsnView

--- app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDsnView.kt
@@ -1,8 +1,20 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.costumwidget.TopAppBar
 import com.example.ucp2.ui.viewmodel.dosen.HomeDsnViewModel
 import com.example.ucp2.ui.viewmodel.dosen.PenyediaDsnViewModel
 
@@ -13,4 +25,41 @@ fun HomeDsnView (
     onBack: () -> Unit,
     onDetailClick: (String) -> Unit = { },
     modifier: Modifier = Modifier
-) {}
+) {
+    Scaffold(
+        modifier= Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .padding(top = 18.dp),
+        topBar = {
+            TopAppBar(
+                judul = "Daftar Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDsn,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dosen",
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeDsnView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
