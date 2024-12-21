c5e72cc13238fa163cc25964790e260e54f4fe08
nauvalachid <nauvalachid@gmail.com>
2024-12-21 12:14:10 +0700

    Menambahkan composable FormMatakuliah untuk form input MataKuliah dengan validasi dan daftar Dosen

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/InsertMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/InsertMkView.kt
@@ -1,2 +1,16 @@
 package com.example.ucp2.ui.view.matakuliah
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.ui.viewmodel.matakuliah.FormErrorState
+import com.example.ucp2.ui.viewmodel.matakuliah.MatakuliahEvent
+
+@Composable
+fun FormMatakuliah(
+    matakuliahEvent: MatakuliahEvent = MatakuliahEvent(),
+    onValueChange: (MatakuliahEvent) -> Unit = {},
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier,
+    listDsn: List<Dosen>
+) {}
