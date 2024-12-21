5ef42c815bef3c93d696d043a0757fd33363dc80
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:00:22 +0700

    Menambahkan DetailMkViewModel dengan SavedStateHandle dan RepositoryMk sebagai dependensi

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMkViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.SavedStateHandle
+import com.example.ucp2.repository.RepositoryMk
+
+class DetailMkViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMk: RepositoryMk,
+
+    )
