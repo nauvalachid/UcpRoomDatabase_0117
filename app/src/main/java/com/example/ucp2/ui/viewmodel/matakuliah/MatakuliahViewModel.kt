ee3e4263af8df9eff186f32855166a26fe61a64c
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:12:45 +0700

    Menambahkan FormErrorState untuk validasi input form mata kuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MatakuliahViewModel.kt
@@ -2,6 +2,20 @@ package com.example.ucp2.ui.viewmodel.matakuliah
 
 import com.example.ucp2.data.entity.MataKuliah
 
+data class FormErrorState(
+    val kode: String? = null,
+    val nama: String? = null,
+    val sks: String? = null,
+    val semester: String? = null,
+    val jenis: String? = null,
+    val dosenpengampu: String? = null,
+){
+    fun isValid(): Boolean {
+        return kode == null && nama == null && sks == null &&
+                semester == null && jenis == null && dosenpengampu == null
+    }
+}
+
 fun MatakuliahEvent.toMatakuliahEntity(): MataKuliah = MataKuliah(
     kode = kode,
     nama = nama,
