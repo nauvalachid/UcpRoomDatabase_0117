468e0d53b72f6e81781f8174d97558cbac5750c6
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:11:08 +0700

    Menambahkan data class MatakuliahEvent untuk merepresentasikan event mata kuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MatakuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/MatakuliahViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+data class MatakuliahEvent(
+    val kode: String = "",
+    val nama: String ="",
+    val sks: String ="",
+    val semester: String ="",
+    val jenis: String ="",
+    val dosenpengampu: String ="",
+)
