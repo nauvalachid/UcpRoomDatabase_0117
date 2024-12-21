cdeb1e0ddc55e387d431aa0f8670a1d94e498d91
nauvalachid <nauvalachid@gmail.com>
2024-12-21 10:57:06 +0700

    Menambahkan kelas KrsApp untuk menyimpan instance ContainerApp

--- app/src/main/java/com/example/ucp2/KrsApp.kt
+++ app/src/main/java/com/example/ucp2/KrsApp.kt
@@ -1,4 +1,16 @@
 package com.example.ucp2
 
-class KrsApp {
+import android.app.Application
+import com.example.ucp2.dependenciesinjection.ContainerApp
+
+class KrsApp : Application(){
+    //Fungsinya untuk menyimpan instance ContainerApp
+    lateinit var containerApp : ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        //Membuat instance ContainerApp
+        containerApp = ContainerApp(this)
+        //instance adalah object yang dibuat dari class
+    }
 }
