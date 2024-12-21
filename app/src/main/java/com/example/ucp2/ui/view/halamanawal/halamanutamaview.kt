8b3b150f790b905f1b2287b3244d1353820fae5d
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:47:52 +0700

    Menambahkan komponen StaticButton dengan gaya khusus

--- app/src/main/java/com/example/ucp2/ui/view/halamanawal/halamanutamaview.kt
+++ app/src/main/java/com/example/ucp2/ui/view/halamanawal/halamanutamaview.kt
@@ -1,2 +1,36 @@
 package com.example.ucp2.ui.view.halamanawal
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun StaticButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Red, // Matching primary purple
+            contentColor = Color.Black
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(32.dp)
+    ) {
+        // Centered text
+        Text(
+            text = text,
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
