84281d6aa2f293696f9870fb8f190a8a32676dbd
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:29:50 +0700

    Menambahkan composable TopAppBar untuk menampilkan toolbar dengan tombol kembali dan judul

--- app/src/main/java/com/example/ucp2/ui/costumwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/costumwidget/TopAppBar.kt
@@ -1,2 +1,12 @@
 package com.example.ucp2.ui.costumwidget
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    modifier: Modifier = Modifier
+) {}
