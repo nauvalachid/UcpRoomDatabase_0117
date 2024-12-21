ffe1189ff8c7c6e4a1b68a6af91810046411864f
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:22:58 +0700

    Menambahkan PenyediaMkViewModel dengan factory untuk inisialisasi MatakuliahViewModel, HomeMkViewModel, DetailMkViewModel, dan UpdateMkViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/PenyediaMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/PenyediaMkViewModel.kt
@@ -1,2 +1,37 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.ui.viewmodel.dosen.krsApp
+
+object PenyediaMkViewModel{
+
+    val Factory = viewModelFactory {
+        initializer {
+            MatakuliahViewModel(
+                krsApp().containerApp.repositoryMk
+            )
+        }
+
+        initializer {
+            HomeMkViewModel(
+                krsApp().containerApp.repositoryMk
+            )
+        }
+
+        initializer {
+            DetailMkViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMk,
+            )
+        }
+
+        initializer {
+            UpdateMkViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMk,
+            )
+        }
+    }
+}
