7856f6b8653d484591c5c212ae03e1a258a96beb
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:27:38 +0700

    Menambahkan composable HomeDsnView untuk menampilkan halaman utama Dosen dengan aksi tambahan

--- app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDsnView.kt
@@ -1,2 +1,16 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.viewmodel.dosen.HomeDsnViewModel
+import com.example.ucp2.ui.viewmodel.dosen.PenyediaDsnViewModel
+
+@Composable
+fun HomeDsnView (
+    viewModel: HomeDsnViewModel = viewModel (factory = PenyediaDsnViewModel. Factory),
+    onAddDsn: () -> Unit = { },
+    onBack: () -> Unit,
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {}
