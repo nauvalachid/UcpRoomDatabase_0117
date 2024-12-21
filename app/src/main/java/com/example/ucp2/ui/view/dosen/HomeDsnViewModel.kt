5a71ee7a296d0acdd58141047a8a9cda19484440
nauvalachid <nauvalachid@gmail.com>
2024-12-21 10:44:38 +0700

    Menambahkan HomeDsnViewModel untuk mengelola data Dosen di halaman utama

--- app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDsnViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDsnViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.lifecycle.ViewModel
+import com.example.ucp2.repository.RepositoryDsn
+
+class HomeDsnViewModel (
+    private val repositoryDsn: RepositoryDsn
+) : ViewModel()
