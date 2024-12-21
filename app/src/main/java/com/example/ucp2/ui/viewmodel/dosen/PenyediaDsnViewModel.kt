51a9e5016f3080ed1d8f32f3c441f314f13b9296
nauvalachid <nauvalachid@gmail.com>
2024-12-21 10:58:21 +0700

    Mengimport KrsApp dari class KrsApp

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/PenyediaDsnViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/PenyediaDsnViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.KrsApp
 
 object PenyediaDsnViewModel{
 
