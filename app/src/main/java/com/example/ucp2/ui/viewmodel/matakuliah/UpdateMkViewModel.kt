5104a89aed7e2f8534147971cd1eca661615a28f
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:20:13 +0700

    Menambahkan fungsi ekstensi untuk konversi MataKuliah ke MkUIState

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/UpdateMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/UpdateMkViewModel.kt
@@ -1,2 +1,7 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import com.example.ucp2.data.entity.MataKuliah
+
+fun MataKuliah. toUIStateMk () : MkUIState = MkUIState (
+    matakuliahEvent = this. toDetailUiEvent (),
+)
