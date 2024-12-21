707a4b960f5b363ef8a4eb5f11544e78b38664c4
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:06:36 +0700

    Menambahkan HomeMkViewModel untuk mengelola data mata kuliah di halaman utama

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMkViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.ViewModel
+import com.example.ucp2.repository.RepositoryMk
+
+class HomeMkViewModel (
+    private val repositoryMk: RepositoryMk
+) : ViewModel()
