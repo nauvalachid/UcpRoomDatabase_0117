25c8451575da7a7fceb38470a67df9d8b1dd60fd
nauvalachid <nauvalachid@gmail.com>
2024-12-21 12:27:19 +0700

    Menambahkan composable UpdateMkView untuk update MataKuliah dengan navigasi dan ViewModel

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/UpdateMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/UpdateMkView.kt
@@ -1,2 +1,19 @@
 package com.example.ucp2.ui.view.matakuliah
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.viewmodel.dosen.HomeDsnViewModel
+import com.example.ucp2.ui.viewmodel.dosen.PenyediaDsnViewModel
+import com.example.ucp2.ui.viewmodel.matakuliah.PenyediaMkViewModel
+import com.example.ucp2.ui.viewmodel.matakuliah.UpdateMkViewModel
+
+@Composable
+fun UpdateMkView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMkViewModel = viewModel(factory = PenyediaMkViewModel.Factory),
+    viewModelDsn: HomeDsnViewModel = viewModel(factory = PenyediaDsnViewModel.Factory),
+
+    ){}
