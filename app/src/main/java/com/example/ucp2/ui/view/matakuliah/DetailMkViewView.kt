e7d331ed39c82793c91ee5d6cbd3d5116a803375
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:52:59 +0700

    Menambahkan composable DeleteConfirmationDialog untuk konfirmasi penghapusan

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/DetailMkViewView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/DetailMkViewView.kt
@@ -1,2 +1,10 @@
 package com.example.ucp2.ui.view.matakuliah
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog (
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier =
+        Modifier
+) {}
