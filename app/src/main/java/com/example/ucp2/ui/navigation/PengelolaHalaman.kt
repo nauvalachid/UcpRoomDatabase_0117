e2bfc853d6d0e5d8862bf66518934a3d0d3eb0fb
nauvalachid <nauvalachid@gmail.com>
2024-12-21 12:33:35 +0700

    Menambahkan composable PengelolaHalaman untuk pengelolaan navigasi dengan NavHostController

--- app/src/main/java/com/example/ucp2/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/PengelolaHalaman.kt
@@ -1,2 +1,12 @@
 package com.example.ucp2.ui.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+){}
