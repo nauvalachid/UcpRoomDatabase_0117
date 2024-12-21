3a2a705c497694b17a36dd0a0318f8edbcfe1da8
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:01:49 +0700

    Menambahkan DetailMkViewModel untuk mengelola detail mata kuliah, dengan fitur loading, error handling, dan penghapusan mata kuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/DetailMkViewModel.kt
@@ -1,10 +1,54 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
 import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.repository.RepositoryMk
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class DetailMkViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMk: RepositoryMk,
 
-    )
+    ) : ViewModel() {
+    private val kode: String = checkNotNull(savedStateHandle[DestinasiMKDetail.KODE])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMk.getMk(kode)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiEvent(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoading = true))
+            kotlinx.coroutines.delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoading = true,
+            ),
+        )
+
+    fun deleteMk() {
+        detailUiState.value.detailUiEvent.toMatakuliahEntity().let {
+            viewModelScope.launch {
+                repositoryMk.deleteMk(it)
+            }
+        }
+    }
+}
