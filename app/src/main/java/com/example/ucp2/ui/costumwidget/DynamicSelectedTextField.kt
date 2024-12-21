4d3af3bba75ee0b2957c1adff5430036514683e0
nauvalachid <nauvalachid@gmail.com>
2024-12-21 12:19:51 +0700

    Menambahkan composable DynamicSelectedTextField untuk dropdown dengan nilai yang dapat dipilih

--- app/src/main/java/com/example/ucp2/ui/costumwidget/DynamicSelectedTextField.kt
+++ app/src/main/java/com/example/ucp2/ui/costumwidget/DynamicSelectedTextField.kt
@@ -1,2 +1,15 @@
 package com.example.ucp2.ui.costumwidget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){}
