98b4a70f5acacf9d27bdf8076a4280cd3e7c3773
nauvalachid <nauvalachid@gmail.com>
2024-12-21 12:03:53 +0700

    Menambahkan composable CardMk untuk menampilkan kartu MataKuliah dengan aksi klik

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/HomeMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/HomeMkView.kt
@@ -1,2 +1,12 @@
 package com.example.ucp2.ui.view.matakuliah
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.ucp2.data.entity.MataKuliah
+
+@Composable
+fun CardMk (
+    mk: MataKuliah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {}
