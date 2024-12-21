fcce52bc4b121af1f0ee754a363dc3116ca00059
nauvalachid <nauvalachid@gmail.com>
2024-12-21 12:23:32 +0700

    mengimport alamat navigasi

--- app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDsnView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/InsertDsnView.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.ucp2.ui.navigation.AlamatNavigasi
 import com.example.ucp2.ui.viewmodel.dosen.DosenEvent
 import com.example.ucp2.ui.viewmodel.dosen.DsnUIState
 import com.example.ucp2.ui.viewmodel.dosen.FormErrorState
