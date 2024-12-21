a1bb094faeab3c87fef2aa0937c8120377a002ea
nauvalachid <nauvalachid@gmail.com>
2024-12-21 10:52:57 +0700

    Menambahkan PenyediaDsnViewModel dengan factory untuk inisialisasi HomeDsnViewModel dan DosenViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/PenyediaDsnViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/dosen/PenyediaDsnViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.ucp2.ui.viewmodel.dosen
 
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaDsnViewModel{
+
+    val Factory = viewModelFactory {
+        initializer {
+            HomeDsnViewModel(
+                krsApp().containerApp.repositoryDsn
+            )
+        }
+
+        initializer {
+            DosenViewModel(
+                krsApp().containerApp.repositoryDsn
+            )
+        }
+
+    }
+}
