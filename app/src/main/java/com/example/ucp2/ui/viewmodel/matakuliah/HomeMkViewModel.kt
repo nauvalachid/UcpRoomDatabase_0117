a942b3ee6b142eecc2a4c773bb1b66b6964d9057
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:08:48 +0700

    Mengimport yang masih merah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMkViewModel.kt
@@ -7,6 +7,11 @@ import com.example.ucp2.repository.RepositoryMk
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMkViewModel (
     private val repositoryMk: RepositoryMk
