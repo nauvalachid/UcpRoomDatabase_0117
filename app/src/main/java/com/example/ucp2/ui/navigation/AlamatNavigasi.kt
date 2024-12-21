79db2c44d99d4dddcbeac41dd38eb2e402425ee9
nauvalachid <nauvalachid@gmail.com>
2024-12-21 12:17:15 +0700

    Menambahkan objek navigasi untuk halaman utama, dosen, MataKuliah, detail, dan update

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -1,2 +1,29 @@
 package com.example.ucp2.ui.navigation
 
+interface AlamatNavigasi{
+    val route : String
+}
+
+object DestinasiHalamanUtama : AlamatNavigasi{
+    override val route = "home"
+}
+
+object DestinasiDosen : AlamatNavigasi {
+    override val route = "dosen"
+}
+
+object DestinasiMataKuliah : AlamatNavigasi {
+    override val route = "matakuliah"
+}
+
+object DestinasiMKDetail : AlamatNavigasi{
+    override val route = "MKdetail"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+
+object DestinasiMKUpdate : AlamatNavigasi{
+    override val route = "MKupdate"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
