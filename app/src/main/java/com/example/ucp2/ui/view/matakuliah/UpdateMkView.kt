c945d1ec6cd40b90c7e180f630fce244994482b3
nauvalachid <nauvalachid@gmail.com>
2024-12-21 12:28:04 +0700

    Menambahkan state dan snackbar untuk UpdateMkView serta mengambil data Dosen dari ViewModel

--- app/src/main/java/com/example/ucp2/ui/view/matakuliah/UpdateMkView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/matakuliah/UpdateMkView.kt
@@ -1,6 +1,11 @@
 package com.example.ucp2.ui.view.matakuliah
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.viewmodel.dosen.HomeDsnViewModel
@@ -16,4 +21,9 @@ fun UpdateMkView(
     viewModel: UpdateMkViewModel = viewModel(factory = PenyediaMkViewModel.Factory),
     viewModelDsn: HomeDsnViewModel = viewModel(factory = PenyediaDsnViewModel.Factory),
 
-    ){}
+    ){
+    val uiState = viewModel.updateUIState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    val dsnList by viewModelDsn.homeUiState.collectAsState()
+    }
