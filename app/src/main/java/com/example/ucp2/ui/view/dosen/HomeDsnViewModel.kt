c727e3a7ddfa941de0847413ec0b026aa30270fc
nauvalachid <nauvalachid@gmail.com>
2024-12-21 10:45:51 +0700

    Menambahkan HomeUiState dengan pengelolaan loading, error, dan data Dosen menggunakan StateFlow

--- app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDsnViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDsnViewModel.kt
@@ -1,8 +1,42 @@
 package com.example.ucp2.ui.view.dosen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.repository.RepositoryDsn
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
 class HomeDsnViewModel (
     private val repositoryDsn: RepositoryDsn
-) : ViewModel()
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryDsn.getAllDsn()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listDsn = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
