6b296d0f05689201d8226f0cc7019069cba9532b
nauvalachid <nauvalachid@gmail.com>
2024-12-21 10:32:44 +0700

    Menambahkan logika validasi untuk field Dosen dan pembaruan manajemen UI state

--- app/src/main/java/com/example/ucp2/ui/view/dosen/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/DosenViewModel.kt
@@ -1,2 +1,28 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.ucp2.repository.RepositoryDsn
+
+class DosenViewModel(private val repositoryDsn: RepositoryDsn) : ViewModel() {
+
+    var uiState by mutableStateOf(DsnUIState())
+
+    fun updateState(dosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent,
+        )
+    }
+
+    fun validateFields(): Boolean {
+        val event = uiState.dosenEvent
+        val errorState = FormErrorState(
+            nidn = if (event.nidn.isNotEmpty()) null else "NIDN tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
