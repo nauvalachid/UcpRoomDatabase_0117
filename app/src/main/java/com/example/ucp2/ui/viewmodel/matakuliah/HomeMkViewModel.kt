a42f5c143f8a5e1fb55053c52103885bad49daa5
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:07:25 +0700

    Menambahkan StateFlow pada HomeMkViewModel untuk mengelola state loading, error, dan daftar mata kuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/HomeMkViewModel.kt
@@ -1,8 +1,42 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.repository.RepositoryMk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 
 class HomeMkViewModel (
     private val repositoryMk: RepositoryMk
-) : ViewModel()
+) : ViewModel() {
+
+    val homeUiState: StateFlow<HomeUiState> = repositoryMk.getAllMk()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMk = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(
+                isLoading = true,
+            )
+        )
+}
