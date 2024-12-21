84a7c224b21e519bf8ce00ce54e8e0783f21c278
nauvalachid <nauvalachid@gmail.com>
2024-12-21 11:20:53 +0700

    Menambahkan UpdateMkViewModel untuk mengelola pembaruan data mata kuliah

--- app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/UpdateMkViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/matakuliah/UpdateMkViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.ucp2.ui.viewmodel.matakuliah
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositoryMk
+
+class UpdateMkViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMk: RepositoryMk
+) : ViewModel()
 
 fun MataKuliah. toUIStateMk () : MkUIState = MkUIState (
     matakuliahEvent = this. toDetailUiEvent (),
